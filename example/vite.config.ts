215fe3d356fb76ed84e56c371f1350721df6dab6
2452327415qh-debug <2452327415qh@gmail.com>
2026-01-24 18:09:38 +0800

    github pages

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [vue()],
   base: "/",
+  build: {
+    outDir: "../docs", // 输出到仓库根目录的 docs
+    emptyOutDir: true,
+  },
 });
